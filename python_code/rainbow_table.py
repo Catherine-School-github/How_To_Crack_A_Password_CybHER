4e0c30d6354f7bcd9c47a1fe0384cfb4eaae6131
Catherine <catherine.hoier@trojans.dsu.edu>
2023-05-23 14:14:41 -0500

    added comments

--- python_code/rainbow_table.py
+++ python_code/rainbow_table.py
@@ -12,23 +12,25 @@ def rand_or_input():
     return decision
 
 
+#gets username. probably doesn't need to be in a functoin but looks clean
 def getting_username():
 
     username = input("Please enter your username: ")
     return username
 
 
+#either transforms the userinput into a hash or randomly assign the hash form the hash_file.txt
 def getting_hash(decision):
 
     if(decision == 1):
-        password = input("Please enter your password: ")
-        hashed_password = sha256(password.encode()).hexdigest() + "\n"
-        print("Your password hashed is: {}".format(hashed_password))
+        password = input("Please enter your password: ")    #gets password
+        hashed_password = sha256(password.encode()).hexdigest() + "\n"  #encodes password into sha256. Adds a newline character because python does that automatically when reading a file
+        print("Your password hashed is: {}".format(hashed_password))    
         return hashed_password
     
     else:
-        hash_file = open("hash_file.txt", 'r')
-        hashed_password = (random.choice(list(hash_file.readlines())))
+        hash_file = open("hash_file.txt", 'r')  #opens hash_file.txt to be used to get a random hash
+        hashed_password = (random.choice(list(hash_file.readlines())))  #casts hash_file.txt as a list so it can be randomly chosen from
         print("Your hashed password is: {}".format(hashed_password))
         hash_file.close()
         return hashed_password
@@ -37,10 +39,10 @@ def getting_hash(decision):
 
 def cracking_hash(hashed_password):
     
-    hash_file = open("hash_file.txt", 'r')
+    hash_file = open("hash_file.txt", 'r')  #opens the hash_file.txt
     
-    for i in hash_file:
-        if (i == hashed_password):
+    for i in hash_file: #while not the end of the file
+        if (i == hashed_password): #if the hashed_password is equal to a password on the list, return True
             return True
         
     return False
